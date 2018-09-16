14608dee031f3960a0d6a2aec777d10fcfbf80a0
Dasha Gurinovich <dashagurinovich425@gmail.com>
2018-09-16 23:38:31 +0300

    Added the demo and full versions. Added the version and IMEI to the layout

--- app/src/main/java/com/example/dashagurinovich/androidapp/MainActivity.kt
+++ app/src/main/java/com/example/dashagurinovich/androidapp/MainActivity.kt
@@ -1,12 +1,46 @@
 package com.example.dashagurinovich.androidapp
 
-import android.support.v7.app.AppCompatActivity
+import android.Manifest
+import android.content.Context
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.support.v4.app.ActivityCompat
+import android.support.v7.app.AppCompatActivity
+import android.telephony.TelephonyManager
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        addPhoneImeiToLayout()
+        addCurrentAppVersionToLayout()
+    }
+
+    private fun addPhoneImeiToLayout(){
+        val telephonyManager = getSystemService(Context.TELEPHONY_SERVICE) as TelephonyManager
+        val permission = ActivityCompat.checkSelfPermission(this,
+                Manifest.permission.READ_PHONE_STATE)
+
+        var imei: String
+        if (permission == PackageManager.PERMISSION_GRANTED) {
+            imei = telephonyManager.deviceId
+            if (imei == null) imei = "No information"
+        }
+        else {
+            imei = "No information"
+        }
+
+        val phoneImeiTextView = findViewById<TextView>(R.id.phoneImeiTextView)
+        phoneImeiTextView.text = imei
+    }
+
+    private fun addCurrentAppVersionToLayout(){
+        val appVersion = BuildConfig.VERSION_NAME
+        val versionTextView = findViewById<TextView>(R.id.versionTextView)
+        versionTextView.text = appVersion
     }
 }
