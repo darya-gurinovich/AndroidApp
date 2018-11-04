d00bfdb8eec9ec1125910449c8cc43124052e8cd
Dasha Gurinovich <dashagurinovich425@gmail.com>
2018-11-04 17:32:04 +0300

    Started the implementation of DrawerNavigation

--- app/src/main/java/com/example/dashagurinovich/androidapp/MainActivity.kt
+++ app/src/main/java/com/example/dashagurinovich/androidapp/MainActivity.kt
@@ -19,8 +19,8 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        addPhoneImeiToLayout()
-        addCurrentAppVersionToLayout()
+       // addPhoneImeiToLayout()
+       // addCurrentAppVersionToLayout()
     }
 
     override fun onRequestPermissionsResult(requestCode: Int,
