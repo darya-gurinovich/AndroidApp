470e3c4f2af841c3d8e15a35963dc746c5774bd8
Dasha Gurinovich <dashagurinovich425@gmail.com>
2018-11-11 17:38:02 +0300

    Added the deep links

--- app/src/main/java/com/example/dashagurinovich/androidapp/fragments/HelpFragment.kt
+++ app/src/main/java/com/example/dashagurinovich/androidapp/fragments/HelpFragment.kt
@@ -8,16 +8,6 @@ import android.view.View
 import android.view.ViewGroup
 import com.example.dashagurinovich.androidapp.R
 
-
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- *
- */
 class HelpFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
